f8716a563f45d7e9e072b74eedf98eed79df18e1
JPTicse <jpticse@gmail.com>
2022-12-05 22:30:50 -0500

    Fixing

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-"""This module starts a flask app on 0.0.0.0:5000"""
-
+"""Initialize a Flask application with state_list, using the Storage"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask, abort, render_template
+from models.state import State
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states")
+def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>")
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for states in storage.all("State").values():
+        if states.id == id:
+            return render_template("9-states.html", states=states)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def tear_down(exception):
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def states():
-    """Renders an HTML page"""
-    states = storage.all("State")
-    return render_template('9-states.html', states=states)
-
-
-@app.route('/states/<_id>', strict_slashes=False)
-def state(_id):
-    """Renders an HTML page"""
-    states = storage.all("State")
-    state = None
-    for st in states.values():
-        if _id == st.id:
-            state = st
-            break
-    return render_template('9-states.html', state=state)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if (__name__ == '__main__'):
+    app.run(host="0.0.0.0", port=5000)
